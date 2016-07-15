6a0528cbcf1c83ac6f99d81a80bec31226b900f2
John Shew <johnshew on twitter>
2016-07-15 12:43:10 -0700

    Added multiple ports

--- Examples/Simple/server.js
+++ Examples/Simple/server.js
@@ -1,14 +1,30 @@
+'use strict'
+
 // Load the http module to create an http server.
 var http = require('http');
 
 // Configure our HTTP server to respond with Hello World to all requests.
-var server = http.createServer(function (request, response) {
+var server = http.createServer( (request, response) => {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.end("Hello World\n");
 });
+
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var logServer = http.createServer( (request, response) => {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Logger Running\n");
+});
+
 
 // Listen on port 8000, IP defaults to 127.0.0.1
 server.listen(8000);
 
 // Put a friendly message on the terminal
 console.log("Server running at http://127.0.0.1:8000/");
+
+// Listen on port 8090, IP defaults to 127.0.0.1
+logServer.listen(8090);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8090/");
